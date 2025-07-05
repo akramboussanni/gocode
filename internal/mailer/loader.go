2fac3ae74df9e7f64575429d35dfc845623e5850
Akram Boussanni <akramboussanni@gmail.com>
2025-07-05 12:30:10 -0400

    wip mailing

--- internal/mailer/loader.go
+++ internal/mailer/loader.go
@@ -1,14 +1,35 @@
 package mailer
 
 import (
+	"embed"
 	"html/template"
+	"os"
 )
 
 var tmplCache = map[string]*template.Template{}
 
-func loadTemplate(name string) (*template.Template, error) {
+//go:embed templates/*
+var embedded embed.FS
+
+func GetTemplate(name string) (*template.Template, error) {
 	if tmpl, ok := tmplCache[name]; ok {
 		return tmpl, nil
 	}
 
+	path := "templates/" + name + ".html"
+	tmpl, err := loadTemplate(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	tmplCache[name] = tmpl
+	return tmpl, nil
+}
+
+func loadTemplate(path string) (*template.Template, error) {
+	if _, err := os.Stat(path); err == nil {
+		return template.ParseFiles(path)
+	}
+	return template.ParseFS(embedded, path)
 }
